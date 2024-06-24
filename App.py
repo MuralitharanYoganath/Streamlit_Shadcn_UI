dd723d00f2029ad93da8275c2dadc4ece86e728c
Muralitharan Yoganath <muraliemailz@gmail.com>
2024-06-24 13:47:02 +0530

    Added to Small Changes of App.py and streamlit shadcn file

--- App.py
+++ App.py
@@ -18,7 +18,13 @@ with st.sidebar:
 job_description = st.text_area("Describe to Job Profile")
 
 # Submit button
-st.button("Submit")
+col1, col2, col3 = st.columns([1, 1, 1])
+with col1:
+    ui.button('Submit', key='btn1')
+with col2:
+    ui.button('View', key='btn2')
+with col3:
+    ui.button('Resources', key='btn3')
 
 # Candidates table
 st.subheader("Candidates")
@@ -34,5 +40,7 @@ df = pd.DataFrame(candidates)
 
 st.dataframe(df)
 
+
 # Schedule interview button
+
 st.button("Schedule interview")  
--- streamlit-shadcn-components/image.py
+++ streamlit-shadcn-components/image.py
@@ -10,14 +10,13 @@ def get_img_as_base64(file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-
 img = get_img_as_base64("image.jpg")
 
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
 background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
-background-size: 180%;
+background-size: 150%;
 background-position: top left;
 background-repeat: no-repeat;
 background-attachment: local;
